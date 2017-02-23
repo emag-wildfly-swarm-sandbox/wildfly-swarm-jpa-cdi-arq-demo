77c9163e269aa1262d6128a4182daead11b78b4c
emag <lanabe.lanabe@gmail.com>
2017-02-24 00:08:05 +0900

    Add Dummy.java to load project-defualts.yml from classpath

--- src/test/java/wildflyswarm/jpa_cdi_arq/EmployeeRepositoryIT.java
+++ src/test/java/wildflyswarm/jpa_cdi_arq/EmployeeRepositoryIT.java
@@ -1,12 +1,9 @@
 package wildflyswarm.jpa_cdi_arq;
 
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
 
 import javax.inject.Inject;
 
@@ -15,17 +12,8 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(Arquillian.class)
-//@DefaultDeployment
+@DefaultDeployment
 public class EmployeeRepositoryIT {
-
-  @Deployment
-  public static Archive createDeployment() {
-    return ShrinkWrap.create(WebArchive.class)
-      .addPackage(Employee.class.getPackage())
-      .addAsResource("project-it.yml", "project-defaults.yml")
-      .addAsResource("META-INF/test-persistence.xml",  "META-INF/persistence.xml")
-      .addAsResource("META-INF/test-load.sql",  "META-INF/test-load.sql");
-  }
 
   @Inject
   private EmployeeRepository sut;
--- src/test/resources/META-INF/test-load.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO employees(id, name) VALUES (1, 'Thom')
-INSERT INTO employees(id, name) VALUES (2, 'Jonny')
-INSERT INTO employees(id, name) VALUES (3, 'Ed')
-INSERT INTO employees(id, name) VALUES (4, 'Colin')
-INSERT INTO employees(id, name) VALUES (5, 'Phil')
